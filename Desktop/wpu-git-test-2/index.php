52f339ae38ec459244d8e4ce38b6d48bb53702f0
farischaai <farischaai@gmail.com>
2025-08-13 10:18:32 +0700

    menambahkan list

--- Desktop/wpu-git-test-2/index.php
+++ Desktop/wpu-git-test-2/index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello world</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi illum dolorem autem harum. Illo iure perferendis voluptatem odio quis quos ipsam nostrum. Facilis magnam rerum sunt reiciendis voluptas reprehenderit! Ad.</p>
+    <ul>
+        <li>1</li>
+        <li>2</li>
+    </ul>
 </body>
 </html>
